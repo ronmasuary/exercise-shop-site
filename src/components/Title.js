06835b3fe30b65f7834dbec1e6b37e81bf09077a
ronmasuary <59368145+ronmasuary@users.noreply.github.com>
2020-07-20 13:51:23 +0300

    Add files via upload

---
+++ src/components/Title.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
+
+export default function Title() {
+    return (
+        <div>
+            <h1>Ron's Shop</h1>
+            
+
+        </div>
+    )
+}
